aad0f68aa3031961702b07049b57f37b9832040a
Abbas Saeed <81560978+asaeed-git@users.noreply.github.com>
2025-07-19 06:40:47 +0000

    02,03 files

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // to use modern JS
+
+// alert("hey") // aise browser mei chalta hai, node mei error aigi
+
+// Readability should be priority
+// avoid coding like this
+// console.log("Hey"); console.log("How are you doing");
+
+//Instead do like this
+console.log("Hey")
+console.log("How are you doing")
+
+// ; and whitespaces both are considered as line-khatm
+console.log(2
+    +
+    3
+); // sirf samjhana ke lie hai ye, ki JS whitespaces avoid kr leti hai, smart hai.
+
+
+
+console.log(typeof 2.5);
+// number is output. This implies ek hi data type (number) mei int, aur decimal both covered hain
+

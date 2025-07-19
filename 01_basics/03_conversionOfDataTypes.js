28536102e6b1c525265068b3d4078ea3411864ac
Abbas Saeed <81560978+asaeed-git@users.noreply.github.com>
2025-07-19 06:41:05 +0000

    03

---
+++ 01_basics/03_conversionOfDataTypes.js
@@ -0,0 +1,42 @@
+// let score = 45
+// console.log(typeof(score));
+
+// let strScore = String(score)
+// console.log(strScore);
+// console.log(typeof strScore);
+
+// let boolScore = Boolean(score)
+// console.log(boolScore);
+// console.log(typeof boolScore);
+
+
+// let score2 = "45"
+// let numScore2 = Number(score2)
+// console.log(typeof numScore2);
+// let score3 = "45a"
+// let numScore3 = Number(score3)
+// console.log(typeof numScore3);
+// console.log(numScore3);
+
+let score4 = "abc"
+let numScore4 = Number(score4)
+console.log(typeof numScore4);
+console.log(numScore4);
+
+// "45" => 45
+// "45ab" => NaN
+// "abc" => NaN
+// null => 0
+//false => 0; true => 1
+// undefined => NaN
+
+
+// let score5 = null
+// let numScore5 = Number(score5)
+// console.log(typeof numScore5);
+// console.log(numScore5);
+// let score6 = undefined
+// let numScore6 = Number(score6)
+// console.log(typeof numScore6);
+// console.log(numScore6);
+

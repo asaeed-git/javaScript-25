97da21c79eb6e4e8f723d7785e5e8a0d6c5c1519
Abbas Saeed <81560978+asaeed-git@users.noreply.github.com>
2025-07-23 11:06:30 +0000

    july23

--- 01_basics/03_conversionOfDataTypes.js
+++ 01_basics/03_conversionOfDataTypes.js
@@ -1,27 +1,27 @@
-// let score = 45
+let score = 45
 // console.log(typeof(score));
 
 // let strScore = String(score)
 // console.log(strScore);
 // console.log(typeof strScore);
 
-// let boolScore = Boolean(score)
-// console.log(boolScore);
-// console.log(typeof boolScore);
+let boolScore = Boolean(score)
+console.log(boolScore);
+console.log(typeof boolScore);
 
 
 // let score2 = "45"
 // let numScore2 = Number(score2)
 // console.log(typeof numScore2);
-// let score3 = "45a"
-// let numScore3 = Number(score3)
+let score3 = "45a"
+let numScore3 = Number(score3)
 // console.log(typeof numScore3);
 // console.log(numScore3);
 
-let score4 = "abc"
-let numScore4 = Number(score4)
-console.log(typeof numScore4);
-console.log(numScore4);
+// let score4 = "abc"
+// let numScore4 = Number(score4)
+// console.log(typeof numScore4);
+// console.log(numScore4);
 
 // "45" => 45
 // "45ab" => NaN
@@ -39,4 +39,15 @@ console.log(numScore4);
 // let numScore6 = Number(score6)
 // console.log(typeof numScore6);
 // console.log(numScore6);
+
+// let x = undefined
+// let strX = String(x)
+// console.log(typeof strX);
+// console.log(strX+"Hey");
+
+let x = undefined
+let boolX= Boolean(x)
+console.log(typeof boolX);
+console.log(boolX);
+
 
